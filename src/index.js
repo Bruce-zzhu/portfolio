f405dcc3fddb6e5cc0d6c418dfa53bfbd72e44c8
Bruce Zhu <76635687+Bruce-zzhu@users.noreply.github.com>
2025-01-31 08:46:04 +1100

    Update v1 base route

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/v1">
     <App />
   </BrowserRouter>,
   document.getElementById('root'),
